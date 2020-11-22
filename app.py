05d7af85b072d41460fde2f083a0d5c7bff04461
Sellers Crisp <sellershcrisp@gmail.com>
2020-11-22 13:54:20 -0500

    Added basic routes for home and game

--- app.py
+++ app.py
@@ -1,3 +1,18 @@
+from flask import Flask, render_template, redirect, request, session
 from boggle import Boggle
 
 boggle_game = Boggle()
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "not0so0secret"
+
+
+@app.route("/home")
+@app.route("/")
+def home():
+    """Start page with start game"""
+
+
+@app.route("/game")
+def game_start():
+    """Game page where boggle (form) is played"""
