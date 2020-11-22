873a28edd0dbb057160a20bbc69f679a7d349248
Sellers Crisp <sellershcrisp@gmail.com>
2020-11-22 16:13:57 -0500

    Updated routes and added session functionality

--- app.py
+++ app.py
@@ -1,18 +1,52 @@
-from flask import Flask, render_template, redirect, request, session
+from flask import Flask, render_template, redirect, request, session, jsonify
+# from flask_debugtoolbar import DebugToolbarExtension
 from boggle import Boggle
 
 boggle_game = Boggle()
 
 app = Flask(__name__)
-app.config["SECRET_KEY"] = "not0so0secret"
+app.config['SECRET_KEY'] = "random-secret1234"
+
+# debug = DebugToolbarExtension(app)
 
 
-@app.route("/home")
-@app.route("/")
-def home():
-    """Start page with start game"""
+@app.route('/')
+def get_home():
+    """ Display board and form on home page """
+
+    board = boggle_game.make_board()
+
+    # Add board to session
+    session['board'] = board
+
+    return render_template('home.html', board=board)
 
 
-@app.route("/game")
-def game_start():
-    """Game page where boggle (form) is played"""
+@app.route('/check-guess')
+def check_guess():
+    """ Checks the guess sent from AJAX """
+
+    # Grab guess from the request args
+    guess = request.args['guess']
+
+    board = session['board']
+
+    # Checks to see if the word is valid
+    result = boggle_game.check_valid_word(board, guess)
+
+    # Returns JSON response
+    return jsonify({'result': result})
+
+
+@app.route('/stats', methods=['POST'])
+def update_stats():
+    """ Updates the stats for the user on the backend """
+
+    score = int(request.json["score"])
+    highscore = session.get("highscore", 0)
+    times_played = session.get("times_played", 0)
+
+    session['times_played'] = times_played + 1
+    session['highscore'] = max(score, highscore)
+
+    return jsonify({'timesPlayed': times_played, 'highscore': highscore})
