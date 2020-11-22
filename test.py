1a8d06df896c5f416992d31aec2fa41cd3a9f034
Sellers Crisp <sellershcrisp@gmail.com>
2020-11-22 16:15:23 -0500

    Finished tests

--- test.py
+++ test.py
@@ -6,5 +6,36 @@ from boggle import Boggle
 
 class FlaskTests(TestCase):
 
-    # TODO -- write tests for every view function / feature!
+    def setUp(self):
+        """Stuff to do before every test."""
 
+        self.client = app.test_client()
+        app.config['TESTING'] = True
+
+    def test_home(self):
+        """Make sure board is in session"""
+
+        with self.client:
+            response = self.client.get('/')
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('board', session)
+
+    def test_check_guess(self):
+        """Make sure sending guess works"""
+
+        with self.client:
+            self.client.get('/')
+            response = self.client.get('/check-guess?guess=to')
+            self.assertEqual(response.status_code, 200)
+            self.assertTrue(response.is_json)
+
+    def test_stats(self):
+        """Make sure times_played and highscore is in session"""
+
+        with self.client:
+            self.client.get('/')
+            response = self.client.post(
+                '/stats', json={'score': '25'})
+            self.assertEqual(response.status_code, 200)
+            self.assertIn('times_played', session)
+            self.assertIn('highscore', session)
